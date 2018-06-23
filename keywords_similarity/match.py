90877f5733933ac806d27588a31c02427dcee4d5
LShostenko <luka.shostenko@gmail.com>
2018-06-23 12:12:38 +0300

    Fix similarity algorithm.

--- keywords_similarity/match.py
+++ keywords_similarity/match.py
@@ -38,16 +38,27 @@ def _group_similarity(similarity_matrix):
         n_1, n_2 = n_2, n_1
 
     total_score = 0.
-    matched_ixs = set()
 
-    for i_1 in range(n_1):
-        for i_2 in np.argsort(similarity_matrix[i_1])[:: -1]:
-            if i_2 not in matched_ixs:
-                matched_ixs.add(i_2)
-                total_score += similarity_matrix[i_1, i_2]
-                break
+    matched_1 = set()
+    matched_2 = set()
 
-    for i_2 in set(range(n_2)) - matched_ixs:
+    sorted_ixs = np.unravel_index(
+        similarity_matrix.asgsort(axis=None)[:: -1],
+        similarity_matrix.shape,
+    )
+
+    for i_1, i_2 in sorted_ixs:
+        if i_1 in matched_1 or i_2 in matched_2:
+            continue
+
+        total_score += similarity_matrix[i_1, i_2]
+        matched_1.add(i_1)
+        matched_2.add(i_2)
+
+        if len(matched_1) == n_1:
+            break
+
+    for i_2 in set(range(n_2)) - matched_2:
         total_score += similarity_matrix[: i_2].max()
 
     return total_score / n_2
