f4adfc3dd0a66db93f222a508cbc20274cc14924
LShostenko <luka.shostenko@gmail.com>
2018-06-24 12:26:22 +0300

    Fix tests.

--- tests/test_match.py
+++ tests/test_match.py
@@ -1,17 +1,17 @@
 import math
 
 import numpy as np
-from keywords_similarity.match import _matching_similarity
+from keywords_similarity.match import _greedy_matching_similarity
 
 
-def test_matching_similarity():
+def test_greedy_matching_similarity():
     similarity_matrix = np.array([
         [0.1, 0.1, 0.2, 1.0],
         [0.5, 0.1, 0.9, 1.0],
         [0.8, 0.2, 0.1, 0.1],
     ])
 
-    res = _matching_similarity(similarity_matrix)
+    res = _greedy_matching_similarity(similarity_matrix)
     expected = (1. + 0.8 + 0.2 + 0.2) / 4
 
     assert math.isclose(res, expected)
