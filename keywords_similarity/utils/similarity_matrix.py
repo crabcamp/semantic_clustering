18cdc0cee34be1ab7621a218bd80191a5688a831
LShostenko <luka.shostenko@gmail.com>
2018-08-16 14:40:02 +0300

    Add algorithms structure.

--- keywords_similarity/utils/similarity_matrix.py
+++ keywords_similarity/utils/similarity_matrix.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-def calculate_similarity_matrix(synsets_1, synsets_2, similarity_function):
-    n_1, n_2 = len(synsets_1), len(synsets_2)
+def calculate_similarity_matrix(group_1, group_2, similarity_function):
+    n_1, n_2 = len(group_1), len(group_2)
 
     similarity_matrix = np.zeros((n_1, n_2))
 
     for i in range(n_1):
         for j in range(n_2):
-            s_i, s_j = synsets_1[i], synsets_2[j]
+            s_i, s_j = group_1[i], group_2[j]
             similarity_matrix[i, j] = similarity_function(s_i, s_j)
 
     return similarity_matrix
