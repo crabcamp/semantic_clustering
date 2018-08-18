392feeb721a1216918d3d1dc94668ea1c1bcbe0c
LShostenko <luka.shostenko@gmail.com>
2018-08-18 10:08:01 +0300

    Import functions directly from the package.

---
+++ keywords_similarity/__init__.py
@@ -0,0 +1,9 @@
+from keywords_similarity.keywords_similarity import (
+    keywords2synsets, keywords_similarity, semantic_keywords_similarity,
+)
+
+__all__ = [
+    'keywords2synsets',
+    'keywords_similarity',
+    'semantic_keywords_similarity',
+]
