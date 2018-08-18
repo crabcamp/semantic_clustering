f07a5b18615ab8591302ef2f44457a77116064cc
LShostenko <luka.shostenko@gmail.com>
2018-08-18 12:27:46 +0300

    Use itertools instead of double for.

--- keywords_similarity/keywords_similarity.py
+++ keywords_similarity/keywords_similarity.py
@@ -1,4 +1,5 @@
 import warnings
+from itertools import product
 
 import numpy as np
 from py_stringmatching import Levenshtein
@@ -12,10 +13,8 @@ def _calculate_similarity_matrix(group_1, group_2, similarity_function):
 
     similarity_matrix = np.zeros((n_1, n_2))
 
-    for i in range(n_1):
-        for j in range(n_2):
-            s_i, s_j = group_1[i], group_2[j]
-            similarity_matrix[i, j] = similarity_function(s_i, s_j)
+    for i, j in product(range(n_1), range(n_2)):
+        similarity_matrix[i, j] = similarity_function(group_1[i], group_2[j])
 
     return similarity_matrix
 
